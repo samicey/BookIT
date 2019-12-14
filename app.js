7f83ed6d6a59c0931663fd946512309645641926
Obikaonu Samuel Chibuike <samuelchibuike22@gmail.com>
2019-12-15 00:31:34 +0100

    Created routes and pwered up the views

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const ejs = require('ejs');
 const path = require('path')
 
 
+
 const app = express()
 
 // Middlewares 
+
 
 // set templating engine as ejs
 app.set('views',path.join(__dirname,'frontEnd/views'));
 app.set('view engine', 'ejs');
 
-// serve staic frontend pages
+// serve static frontend pages
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname,'frontEnd')))
 
@@ -21,6 +23,16 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
+// donate book route
+app.get('/DonateBook',(req,res)=>{
+    res.render('donateBook')
+})
+app.get('/SignIn',(req,res)=>{
+    res.render('registerSignIn')
+})
+app.get('/Register',(req,res)=>{
+    res.render('registerSignIn')
+})
 app.listen(3000,()=>{
-           console.log('App is running in port 3000',path.join(__dirname,'frontEnd/views'))
+           console.log('App is running in port 3000')
            })
